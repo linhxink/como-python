e0be648143bbfabdd4cc73f27eff058fb0c5a5fe
linhxink <tnlinh.ng@gmail.com>
2023-04-02 11:02:20 +0200

    pridala jsem krychli a kvadr

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st 
 
-tvar = st.radio("vyber si tvar", ["ctverec", "obdelnik", "kružnice"])
+st.header ("Moje aplikace") 
+
+tvar = st.radio("vyber si tvar", ["ctverec", "obdelnik", "kružnice", "krychle", "kvádr"])
 
 if tvar == "ctverec":
     strana = st.number_input("zadejte stranu ctverce")
@@ -15,4 +17,13 @@ elif tvar == "kružnice":
     polomer = st.number_input("zadejte polomer kruznice")
     st.text(f"obvod kružnice je {2*3.14*polomer} a obsah {3.14*polomer**2}")
     
-st.header ("Moje aplikace")
+elif tvar == "krychle":
+    strana_krychle = st.number_input("zadejte stranu krychle")
+    st.text(f"povrch krychle je {6*strana_krychle**2} a objem je {strana_krychle**3}")
+
+elif tvar == "kvádr":
+    strana_kvadru = st.number_input("zadejte stranu a")
+    strana_kvadru2 = st.number_input("zadejte stranu b")
+    strana_kvadru3 = st.number_input("zadejte stranu c")
+    st.text(f"povrch kvadru je {2*(strana_kvadru*strana_kvadru2+strana_kvadru*strana_kvadru3+strana_kvadru2*strana_kvadru3)} a objem je {strana_kvadru*strana_kvadru2*strana_kvadru3}")
+    
