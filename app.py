a81ddb87de72f886530c9317b10e52e6651da397
linhxink <129649830+linhxink@users.noreply.github.com>
2023-05-07 13:45:18 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,29 +1,45 @@
-import streamlit as st 
+import streamlit as st
+import pandas as pd
+import datetime
 
-st.header ("Moje aplikace") 
+st.title("Konvertor peněz")
+list_men = pd.read_csv("physical_currency_list.csv")
+list_men_final = list_men["currency code"]
+datum = st.date_input("Zadejte datum konverce měny", datetime.date(2023, 5, 7))
+datum = datum.strftime('%Y-%m-%d')
 
-tvar = st.radio("vyber si tvar", ["ctverec", "obdelnik", "kružnice", "krychle", "kvádr"])
+col1, col2 = st.columns(2)
 
-if tvar == "ctverec":
-    strana = st.number_input("zadejte stranu ctverce")
-    st.text(f"obvod ctverce je {strana*4} a obsah je {strana**2}")
+with col1:
+    mena1 = st.selectbox("Vyber si měnu kterou chceš konvertovat", list_men_final, 142)
 
-elif tvar == "obdelnik":
-    strana_a = st.number_input("zadejte stranu a: ")
-    strana_b = st.number_input("zadejte stranu b: ")
-    st.text(f"obvod je {2*strana_a + 2*strana_b} a obsah {strana_b*strana_a}")
+with col2:
+    castka = st.number_input("Kolik peněz chcete vyměnit", min_value= 0)
 
-elif tvar == "kružnice": 
-    polomer = st.number_input("zadejte polomer kruznice")
-    st.text(f"obvod kružnice je {2*3.14*polomer} a obsah {3.14*polomer**2}")
+mena2 = st.selectbox("Do jaké měny chcete konvertovat", list_men_final, 34)
+
+alpha_outputsize = 'full'
+alpha_apikey = 'JCYUNJJG1FH62A97'
+
+btn_konvertovat = st.button("Konvertovat")
+if btn_konvertovat:
+    url = f'https://www.alphavantage.co/query?function=FX_DAILY&from_symbol={mena1}&to_symbol={mena2}&outputsize={alpha_outputsize}&apikey={alpha_apikey}&datatype=csv'
+    tabulka = pd.read_csv(url)
+    tabulka = tabulka[["timestamp", "close"]]
+    konverzni_datum = max(tabulka[tabulka["timestamp"] < datum]["timestamp"])
+    kurz = tabulka.loc[tabulka["timestamp"] == konverzni_datum, "close"].values[0]
+
+    final_castka = kurz*castka
+    zaok = round(final_castka, 2)
     
-elif tvar == "krychle":
-    strana_krychle = st.number_input("zadejte stranu krychle")
-    st.text(f"povrch krychle je {6*strana_krychle**2} a objem je {strana_krychle**3}")
-
-elif tvar == "kvádr":
-    strana_kvadru = st.number_input("zadejte stranu a")
-    strana_kvadru2 = st.number_input("zadejte stranu b")
-    strana_kvadru3 = st.number_input("zadejte stranu c")
-    st.text(f"povrch kvadru je {2*(strana_kvadru*strana_kvadru2+strana_kvadru*strana_kvadru3+strana_kvadru2*strana_kvadru3)} a objem je {strana_kvadru*strana_kvadru2*strana_kvadru3}")
+    col3, col4 = st.columns(2)
     
+    col3.metric("finalní částka", zaok)
+
+    col4.metric(f"1 {mena1} = ", zaok/castka)
+
+    
+
+
+
+
